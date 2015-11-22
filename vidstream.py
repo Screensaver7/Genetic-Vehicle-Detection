4dd6a63898f79fb58ad722aa9c5059673bbd2348
SCRN <screensaver7@gmail.com>
2015-11-21 22:04:00 -0500

    Updated test stream
    Canny edge placeholder

--- vidstream.py
+++ vidstream.py
@@ -2,14 +2,24 @@ import cv2
 import scipy as sp
 
 cap = cv2.VideoCapture()
-cap.open('rtmp://itsvideo.arlingtonva.us:8001/live/cam36.stream')
+cap.open('rtmp://170.93.143.139:1935/rtplive/13015dbd01210075004d823633235daa')
+
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)*1.5)
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)*1.5)
+
+cv2.namedWindow("Stream", cv2.WINDOW_NORMAL)
+cv2.resizeWindow("Stream", w, h)
 
 while(cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
-        #img1 = sp.zeros(frame.shape, sp.uint8)
-        cv2.imshow("Stream", frame)
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+        img1 = sp.zeros(frame.shape, sp.uint8)
+        img1 = cv2.GaussianBlur(frame, (3, 3), 7)
+        img2 = cv2.Canny(img1, 20, 90)
+        cv2.imshow("Stream", img2)
+        if cv2.waitKey(67) & 0xFF == ord('q'):
             break
     else: break
- 
+    
+cap.release()
+cv2.destroyAllWindows()
