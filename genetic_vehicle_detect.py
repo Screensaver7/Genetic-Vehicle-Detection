75ae1bff1f513a7de9afaeeb29946959fb677b7c
SCRN <screensaver7@gmail.com>
2015-12-12 15:40:14 -0500

    parameter checks

--- genetic_vehicle_detect.py
+++ genetic_vehicle_detect.py
@@ -206,6 +206,9 @@ def generateP(poparr, pop):
     return rtn
 
 if __name__ == "__main__":
+    if (len(argv) < 3):
+        print "Not enough parameters entered."
+        exit(1)
     gen = int(argv[1])
     pop = int(argv[2])
     random.seed(datetime.now())
